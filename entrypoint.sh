3cd5a189321ed883bf7594cb090015db2fba785b
Michael <michaelkk42@gmail.com>
2021-06-25 15:22:04 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,4 @@ if [ -z "${TARGET}" ]; then
     TARGET = "default"
 fi
 
-firebase use ${TARGET}
-
-firebase deploy --token ${FIREBASE_TOKEN} --only functions
+firebase deploy --token ${FIREBASE_TOKEN} -P ${TARGET} --only functions
